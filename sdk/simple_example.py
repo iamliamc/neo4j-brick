b1cc5f9148f42c9551c6456526c36ad9cc511c89
Liam Considine <liam@spectral.energy>
2021-01-03 13:02:25 -0500

    Use official BRICK example

--- sdk/simple_example.py
+++ sdk/simple_example.py
@@ -2,6 +2,10 @@ from graph_connection import TopologyGraphDriver
 
 neo4j_topology = TopologyGraphDriver(uri="neo4j://localhost:7687", password="", user="")
 
+# Clean-up just to be sure...
+neo4j_topology.destroy_graph()
+
+
 neo4j_topology.initalize_neo4j_configuration()
 
 load_ontology = True
@@ -10,16 +14,18 @@ load_ontology = True
 if not neo4j_topology.is_brick_ontology_loaded() and load_ontology:
     neo4j_topology.load_brick_ontology()
 
-# Clean-up just to be sure...
-# neo4j_topology.destroy_graph()
-
 # Load TTL from file system while running Neo4j in Docker requires setting up some shared directories therefore reference raw file on repo
-input_turtle = "https://raw.githubusercontent.com/iamliamc/neo4j-brick/main/examples/custom_brick_v11_sample_graph.ttl"
+
+# input_turtle = "https://raw.githubusercontent.com/iamliamc/neo4j-brick/main/examples/custom_brick_v11_sample_graph.ttl"
+
+input_turtle = (
+    "https://raw.githubusercontent.com/BrickSchema/Brick/master/examples/rice_brick.ttl"
+)
 
 neo4j_topology.load_ttl_file(input_turtle)
 
 with neo4j_topology.driver.session() as session:
-    results = session.run("MATCH (n:Air_Handler_Unit)-[z]->(o) return n, z, o")
+    results = session.run("MATCH (n:AHU)-[z]->(o) return n, z, o")
     ahus = [record["n"] for record in results]
     relationships = [record["z"] for record in results]
     related_nodes = [record["o"] for record in results]
